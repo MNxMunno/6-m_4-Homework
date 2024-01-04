36bde7f54850ef65ee5f16c728028c4b38e150e1
MNxMunno <bilolhuzayfa@gmmail.com>
2024-01-04 07:34:47 +0500

    new project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Add from "./components/Add";
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Add />
+      </>
+    );
+  }
+}
+
+export default App;
